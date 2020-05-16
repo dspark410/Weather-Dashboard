eb11f922a43a3dd273ca541636cdd0afb5d6b844
mistahpig <dspark410@gmail.com>
2020-05-16 14:45:22 -0700

    adds gets localstorage

--- script.js
+++ script.js
@@ -1,82 +1,76 @@
 $(document).ready(function () {
 
-//var APIKey = "28c82c982b5d989f68dfe35b65a9e386"
-//var APIKey = "19b63f6a9e10dc5a731779766a775baa"
-var APIKey = "166a433c57516f51dfab1f7edaed8413"
+  var getIt = []
 
-$(".btn").on("click", function(){
+  getStoredWeather()
 
-  var city = $("#city").val().trim();
-  
-  if (!city) {
-    return
-  }
+  var APIKey = "166a433c57516f51dfab1f7edaed8413"
 
-  $(".hide").removeClass("hide")
-  getWeather(city)
-  
-  var li = $("<li>")
-  li.addClass("list-group-item")
-  li.addClass("saved-city")
-  li.attr("data-city", city)
-  var input = li.html(city)
-  $(".list-group").append(input)
+  $(".btn").on("click", function () {
 
-});
+    var city = $("#city").val().trim();
 
-$(".list-group").on("click", ".saved-city", function(){
-  getWeather($(this).attr("data-city"))
+    if (!city) {
+      return
+    }
 
-  getStoredWeather(city)
-})
+    $(".hide").removeClass("hide")
+    getWeather(city)
 
-function getStoredWeather(city) {
-  JSON.parse(localStorage.getItem(city))
-  
-}
+  });
 
-function getWeather(city) {
-  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
+  $(".list-group").on("click", ".saved-city", function () {
+    getWeather($(this).attr("data-city"))
+    $(".hide").removeClass("hide")
   })
-    .then(function(response) {
+
+  function getStoredWeather() {
+    getIt = JSON.parse(localStorage.getItem("city")) || []
+
+    console.log(getIt)
+    for (var i = 0; i < getIt.length; i++) {
+      var li = $("<li>")
+      li.addClass("list-group-item")
+      li.addClass("saved-city")
+      li.attr("data-city", getIt[i])
+      var input = li.html(getIt[i])
+      $(".list-group").append(input)
+    }
+  }
+  function getWeather(city) {
+    var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    })
+      .then(function (response) {
 
         console.log(queryURL)
 
-        $("#show-city").html(response.name)     
+        $("#show-city").html(response.name)
         var date = $("<span>")
         var showDate = date.html(" &#40;" + moment().format("L") + "&#41;")
         $("#show-city").append(showDate)
-        var iconCode = response.weather[0].icon 
+        var iconCode = response.weather[0].icon
         var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
         var img = $("<img>")
         var showImg = img.attr("src", iconURL)
         $("#show-city").append(showImg)
-  
-        $("#temperature").html("Temperature: " + response.main.temp.toFixed(1) + " &degF")
-        $("#humidity").html("Humidity: " + response.main.humidity +"%")
-        $("#windspeed").html("Windspeed: " + response.wind.speed + " MPH")
 
-        var values = {
-          "date": moment().format("L"),
-          "icon": response.weather[0].icon, 
-          "temp": response.main.temp,
-          "humidity": response.main.humidity,
-          "windspeed": response.wind.speed,
-        }
-        
+        $("#temperature").html("Temperature: " + response.main.temp.toFixed(1) + " &degF")
+        $("#humidity").html("Humidity: " + response.main.humidity + "%")
+        $("#windspeed").html("Windspeed: " + response.wind.speed + " MPH")
+      
         var longitude = response.coord.lon
         var latitude = response.coord.lat
 
-        var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude  + "&lon=" + longitude + "&appid=" + APIKey;
+        var queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIKey;
 
         $.ajax({
           url: queryURL2,
           method: "GET",
         })
-          .then(function(response) {
+          .then(function (response) {
             console.log(queryURL2)
 
             $("#uv-index").html(response.value)
@@ -94,48 +88,55 @@ function getWeather(city) {
             if (uvIndex < 11) {
               $("#uv-index").addClass("red")
             }
-            values.uv_index = uvIndex
-            
-            
-        })   
+            if (uvIndex > 11) {
+              $("#uv-index").addClass("purple")
+            }
+
+          })
+
         var queryURL3 = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
 
         $.ajax({
           url: queryURL3,
           method: "GET",
         })
-          .then(function(response) {
-              
-              console.log(queryURL3)
-    
-              var counter = 1
-    
-              for (var i = 8; i < response.list.length; i+=7) {
-              var showFutureDate = moment(response.list[i].dt *1000).format("L")
+          .then(function (response) {
+
+            console.log(queryURL3)
+            console.log(response)
+            var counter = 1
+
+            for (var i = 8; i < response.list.length; i += 7) {
+              var showFutureDate = moment(response.list[i].dt * 1000).format("L")
               $("#forecast-date" + counter).html(showFutureDate)
-        
+
               var iconCode = response.list[i].weather[0].icon
               var iconURL = "http://openweathermap.org/img/w/" + iconCode + ".png"
               var img = $("<img>")
               var showImg = img.attr("src", iconURL)
               $("#forecast-date" + counter).append(showImg)
-    
-              $("#forecast-temp" + counter).html("Temp: " + response.list[i].main.temp.toFixed(2) + " &degF")
-              $("#forecast-humidity" + counter).html("Humidity: " + response.list[i].main.humidity +"%") 
-                
-              counter += 1
-              
-              values.forecastdate = moment(response.list[i].dt *1000).format("L")
-              values.forecasticon = response.list[i].weather[0].icon
-              values.forecasttemp = response.list[i].main.temp
-              values.forecasthumidity = response.list[i].main.humidity
 
-              localStorage.setItem(city, JSON.stringify(values))
-              
-              }
-          });           
-    }); 
-    
-   
-}
+              $("#forecast-temp" + counter).html("Temp: " + response.list[i].main.temp.toFixed(2) + " &degF")
+              $("#forecast-humidity" + counter).html("Humidity: " + response.list[i].main.humidity + "%")
+
+              counter += 1
+
+            }
+
+            getIt.push(city)
+
+            localStorage.setItem("city", JSON.stringify(getIt))
+
+            var li = $("<li>")
+            li.addClass("list-group-item")
+            li.addClass("saved-city")
+            li.attr("data-city", city)
+            var input = li.html(city)
+            $(".list-group").append(input)
+          });
+
+      });
+  }
 })
+
+
